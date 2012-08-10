29316aafc15b613bcb067312bdd66bda538d1e57
Steven Shiau <steven@nchc.org.tw>
2012-08-10 13:48:28 +0800

    Read version number from debian/changelog, not RPM spec file.

--- toolbox/make-deb.sh
+++ toolbox/make-deb.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Steven Shiau <steven _at_ nchc org tw)
+
+# Settings
 PKG="clonezilla"
-SPEC_FILE="$PKG.spec"
 
 set -e
-#
-[ ! -f "$SPEC_FILE" ] && echo "Can NOT find spec file $SPEC_FILE" && exit 1
 
 #
-VER=`grep ^Version $SPEC_FILE |sed -e "s/\t/ /g" -e "s/ \+/ /g" |cut  -d":" -f2 |tr -d " "`
+VER="$(LC_ALL=C head -n 1 debian/changelog  | grep -i "^${PKG}" | grep -E -o "\(.*\)" | sed -r -e "s/\(//g" -e "s/\)//g" | cut -d"-" -f1)"
+[ -z "$VER" ] && echo "No version found in debian/changelog! Program terminated!"
 echo "VER: $VER"
 
 #
@@ -16,7 +16,7 @@ TARBALL=${PKG}-${VER}.tar.bz2
 TARBALL_ORIG=${PKG}_${VER}.orig.tar.bz2
 
 # check
-[ ! -f "$TARBALL" ] && echo "Can NOT find file $TARBALL! Did you forget to update the rdate in file $PKG.spec ? Program Stop!!!" && exit 1
+[ ! -f "$TARBALL" ] && echo "Can NOT find file $TARBALL! Program Stop!!!" && exit 1
 
 # mkdir for build
 rm -rf debforge
