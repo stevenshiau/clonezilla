86fbb812b2eb512e35bde8175c65d99418960245
Steven Shiau <steven@nchc.org.tw>
2012-08-10 13:38:37 +0800

    packit.sh now read version number from debian/changelog.

--- toolbox/packit.sh
+++ toolbox/packit.sh
@@ -1,42 +1,38 @@
 #!/bin/bash
+# generate the ChangeLog.txt and create the tarball
+
+# Settings
 PKG="clonezilla"
-RPMBUILD="${HOME}/rpmbuild/"
-SPEC_FILE="$PKG.spec"
+FILES_DIRS="Makefile clonezilla.spec conf doc samples sbin bin scripts setup prerun postrun" 
 
+set -e
 #
-[ ! -f "$SPEC_FILE" ] && echo "Can NOT find spec file $SPEC_FILE" && exit 1
+VER="$(LC_ALL=C head -n 1 debian/changelog  | grep -i "^${PKG}" | grep -E -o "\(.*\)" | sed -r -e "s/\(//g" -e "s/\)//g" | cut -d"-" -f1)"
+[ -z "$VER" ] && echo "No version found in debian/changelog! Program terminated!"
+echo "VER: $VER"
 
-# Create the changelog file
-mkdir -p doc
-#
-[ -f "doc/ChangeLog.txt" ] && rm -f doc/ChangeLog.txt
-line_begin="$(grep -n "%changelog" $SPEC_FILE | awk -F":" '{print $1}')"
-line_eng="$(wc -l $SPEC_FILE | awk -F" " '{print $1}')"
-lines=$(($line_eng - $line_begin))
+# Create the ChangeLog file
 cat <<EOF > doc/ChangeLog.txt
-Clonezilla, the opensource clone system
-Author: Steven Shiau <steven _at_ nchc org tw>, Blake, Kuo-Lien Huang (klhaung _at_ gmail com), Ceasar Sun (ceasar _at_ nchc org tw), Jazz Wang (jazz _at_ nchc org tw) and Thomas Tsai (thomas _at_ nchc org tw)
+Clonezilla, the opensource clone system.
+Authors: Steven Shiau <steven _at_ nchc org tw>, Blake, Kuo-Lien Huang (klhaung _at_ gmail com), Ceasar Sun (ceasar _at_ nchc org tw), Jazz Wang (jazz _at_ nchc org tw) and Thomas Tsai (thomas _at_ nchc org tw)
 License: GPL
 http://clonezilla.org
+http://clonezilla.sourceforge.net
 http://clonezilla.nchc.org.tw
 
 EOF
 
-tail -n $lines $SPEC_FILE >> doc/ChangeLog.txt
+cat debian/changelog >> doc/ChangeLog.txt
+
+td="${PKG}-${VER}"
 
 #
-VER=`grep ^Version $SPEC_FILE |sed -e "s/\t/ /g" -e "s/ \+/ /g" |cut  -d":" -f2 |tr -d " "`
-echo "VER: $VER"
-
-td="$PKG-$VER"
-#check if necessary files exist...
-[ -f $SPEC_FILE ] || exit 0
 [ -d "$td" ] && rm -rf $td
-# Clean stale files in debian
-rm -rf $PKG/debian/{drbl,tmp}
 mkdir -p $td
-cp -ar Makefile clonezilla.spec conf doc samples sbin bin scripts setup prerun postrun $td/
+# Clean stale files in debian
+rm -rf debian/{drbl,tmp}
+cp -ar $FILES_DIRS $td/
 
+echo $VER > $td/doc/VERSION
 tar cvjf $td.tar.bz2 --owner=root --group=root $td
 rm -rf $td
-[ -f $TARBALL ] || exit 0
